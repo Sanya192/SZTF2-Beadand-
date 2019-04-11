d1da9f4e9442647d60a2ce8b78633a3138ebc4bb
Sanya192 <sanyika192@live.com>
2019-04-12 00:50:14 +0200

    faszom

--- SZTF2 Beadandó/Program.cs
+++ SZTF2 Beadandó/Program.cs
@@ -12,7 +12,7 @@ namespace SZTF2_Beadandó
     {
         static void Main(string[] args) {
             GlobalSettings.Init();
-            var temp = new LocsoloFa(new Locsolo(10000,3));
+            var temp = new LocsoloFa(new Locsolo(10,3));
             var temp2 = temp.Select(p => p).ToArray();
             foreach (var item in temp) {
                 Console.WriteLine(item);
@@ -31,17 +31,96 @@ namespace SZTF2_Beadandó
             this.gyökér = gyökér;
         }
 
-        public IEnumerator<VizesBlokk> GetEnumerator() {
-            return Bejaro(gyökér);
+        public IEnumerator GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+
+        IEnumerator<VizesBlokk> IEnumerable<VizesBlokk>.GetEnumerator()
+        {
+            return new LocsolaFaIenumarator(gyökér);
+        }
+    }
+    class LocsolaFaIenumarator : IEnumerator<VizesBlokk>
+    {
+        Locsolo gyökér;
+        VizesBlokk jelenlegi;
+        bool used;
+        public LocsolaFaIenumarator(Locsolo gyökér)
+        {
+            this.gyökér = gyökér;
+            used = false;
+        }
+
+        public VizesBlokk Current => jelenlegi;
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            gyökér = null;
+            jelenlegi = null;
             
         }
-        public IEnumerator<VizesBlokk> Bejaro(VizesBlokk bejaro) {
-             {
-                
+
+        public bool MoveNext()
+        {
+            if (!used)
+            {
+                used = true;
+                jelenlegi = gyökér;
+                return true;
             }
+            else
+            {
+                try
+                {
+                    foreach (var item in (jelenlegi as Locsolo).Kivezetes)
+                    {
+                        jelenlegi = item;
+                        return true;
+                    }
+                }
+                catch (NullReferenceException)
+                {
+                    try
+                    {
+                        if (jelenlegi.GetType() == typeof(Palánta))
+                        {
+                            jelenlegi = null;
+                            return false;
+                        }
+                    }
+                    catch (NullReferenceException)
+                    {
+
+                        return false;
+
+                    }
+                }
+
+            }
+            return false;
         }
-        IEnumerator IEnumerable.GetEnumerator() {
-            return GetEnumerator();
+        public IEnumerable<VizesBlokk> Bejaro(VizesBlokk bejaro)
+        {
+            if (typeof(Palánta)==bejaro.GetType())
+            {
+                
+                yield return bejaro;
+            }
+            else
+            foreach (var item in (bejaro as Locsolo).Kivezetes)
+            {
+                    
+               yield return Bejaro(item) as VizesBlokk;
+            }
+            
+        }
+        public void Reset()
+        {
+            used = false;
         }
     }
 }
+
